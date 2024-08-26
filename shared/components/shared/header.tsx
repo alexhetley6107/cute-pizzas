2209659fa440b99d0fc82d24acde638d04316be0
aleksSiema <aleks@siema.com.ua>
2024-08-26 20:41:18 +0300

    feat: cart fe

--- shared/components/shared/header.tsx
+++ shared/components/shared/header.tsx
@@ -7,6 +7,7 @@ import { cn } from '@/shared/lib/utils';
 import { Container } from './container';
 import Link from 'next/link';
 import { SearchInput } from './search-input';
+import { CartButton } from './cart-button';
 
 interface Props {
   className?: string;
@@ -36,15 +37,7 @@ export const Header: React.FC<Props> = ({ className }) => {
             Войти
           </Button>
 
-          <Button className="group relative">
-            <b>520 ₽</b>
-            <span className="h-full w-[1px] bg-white/30 mx-3" />
-            <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
-              <ShoppingCart size={20} className="relative" strokeWidth={2} />
-              <b>3</b>
-            </div>
-            <ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
-          </Button>
+          <CartButton />
         </div>
       </Container>
     </header>
