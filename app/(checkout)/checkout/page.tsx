556ff78ad29882fab3ddc962f93f8b1c6f703c3f
aleksSiema <aleks@siema.com.ua>
2024-08-31 19:29:58 +0300

    feat: checkout page

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -1,19 +1,69 @@
 'use client';
 
-import { Container, Title } from '@/shared/components/shared';
-import { Textarea } from '@/shared/components/ui';
+import {
+  CheckoutAddressForm,
+  CheckoutCart,
+  CheckoutPersonalForm,
+  CheckoutSidebar,
+  Container,
+  Title,
+} from '@/shared/components/shared';
 import { useCart } from '@/shared/hooks';
 import React from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { checkoutFormSchema, CheckoutFormValues } from '@/shared/constants';
+import toast from 'react-hot-toast';
 
 export default function CheckoutPage() {
   const [submitting, setSubmitting] = React.useState(false);
   const { totalAmount, updateItemQuantity, items, removeCartItem, loading } = useCart();
 
+  const form = useForm<CheckoutFormValues>({
+    resolver: zodResolver(checkoutFormSchema),
+    defaultValues: {
+      email: '',
+      firstName: '',
+      lastName: '',
+      phone: '',
+      address: '',
+      comment: '',
+    },
+  });
+
+  const onClickCountButton = (id: number, quantity: number, type: 'plus' | 'minus') => {
+    const newQuantity = type === 'plus' ? quantity + 1 : quantity - 1;
+    updateItemQuantity(id, newQuantity);
+  };
+
+  const onSubmit = async (data: CheckoutFormValues) => {
+    console.log(data);
+  };
+
   return (
     <Container className="mt-10">
       <Title text="Оформление заказа" className="font-extrabold mb-8 text-[36px]" />
 
-      <Textarea rows={5} />
+      <FormProvider {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="flex gap-10">
+            <div className="flex flex-col gap-10 flex-1 mb-20">
+              <CheckoutCart
+                onClickCountButton={onClickCountButton}
+                removeCartItem={removeCartItem}
+                items={items}
+                loading={loading}
+              />
+              <CheckoutPersonalForm className={loading ? 'opacity-40 pointer-events-none' : ''} />
+              <CheckoutAddressForm className={loading ? 'opacity-40 pointer-events-none' : ''} />
+            </div>
+
+            <div className="w-[450px]">
+              <CheckoutSidebar totalAmount={totalAmount} loading={loading || submitting} />
+            </div>
+          </div>
+        </form>
+      </FormProvider>
     </Container>
   );
 }
