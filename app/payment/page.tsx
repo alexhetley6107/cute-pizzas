3a89b69ce2c27bde2f0731ac39b924aa9adc063a
aleksSiema <aleks@siema.com.ua>
2024-10-07 20:18:56 +0300

    feat: payment layout

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,5 +1,53 @@
-import { prisma } from '@/prisma/prisma-client';
+'use client';
 
-export default async function PaymentPage({ params: { id } }: { params: { id: string } }) {
-  return <>Payment</>;
+import { FormInput, Title } from '@/shared/components/shared';
+import { Button } from '@/shared/components/ui';
+import { paymentFormSchema, PaymentFormValues } from '@/shared/constants';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FormProvider, useForm } from 'react-hook-form';
+
+export default function PaymentPage() {
+  const form = useForm<PaymentFormValues>({
+    resolver: zodResolver(paymentFormSchema),
+    defaultValues: {
+      accountNumber: '',
+      expiringDate: '',
+      cvv: '',
+    },
+  });
+
+  const onSubmit = () => {};
+
+  return (
+    <FormProvider {...form}>
+      <div className="h-screen flex flex-col justify-center items-center px-5">
+        <div className="w-full lg:w-[450px]">
+          <form onSubmit={form.handleSubmit(onSubmit)} className="">
+            <div className="flex flex-col gap-5">
+              <Title text="Payment" className="font-extrabold mb-2  text-[22px] md:text-[36px]" />
+              <FormInput
+                type="number"
+                name="accountNumber"
+                className="text-base"
+                placeholder="Account Number"
+              />
+
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-5">
+                <FormInput
+                  type="number"
+                  name="expiringDate"
+                  className="text-base"
+                  placeholder="Expiring date"
+                />
+                <FormInput type="number" name="cvv" className="text-base" placeholder="CVV" />
+              </div>
+              <Button type="submit" className="w-full h-14 rounded-2xl text-base font-bold">
+                Pay 57
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </FormProvider>
+  );
 }
--- shared/constants/checkout-form-schema.ts
+++ shared/constants/checkout-form-schema.ts
@@ -1,12 +1,23 @@
 import { z } from 'zod';
 
 export const checkoutFormSchema = z.object({
-  firstName: z.string().min(2, { message: 'Имя должно содержать не менее 2-х символов' }),
-  lastName: z.string().min(2, { message: 'Фамилия должна содержать не менее 2-х символов' }),
-  email: z.string().email({ message: 'Введите корректную почту' }),
-  phone: z.string().min(10, { message: 'Введите корректный номер телефона' }),
-  address: z.string().min(5, { message: 'Введите корректный адрес' }),
+  firstName: z.string().min(2, { message: 'The name must contain at least 2 characters' }),
+  lastName: z.string().min(2, { message: 'The last name must contain at least 2 characters.' }),
+  email: z.string().email({ message: 'Please enter a valid email address' }),
+  phone: z.string().min(10, { message: 'Please enter a valid phone number' }),
+  address: z.string().min(5, { message: 'Please enter a valid address' }),
   comment: z.string().optional(),
 });
 
+export const paymentFormSchema = z.object({
+  accountNumber: z
+    .string()
+    .min(16, { message: 'The account number must contain at least 16 characters' }),
+  expiringDate: z
+    .string()
+    .min(4, { message: 'The expiring date must contain at least 5 characters.' }),
+  cvv: z.string().min(3, { message: 'The cvv must contain at least 5 characters.' }),
+});
+
 export type CheckoutFormValues = z.infer<typeof checkoutFormSchema>;
+export type PaymentFormValues = z.infer<typeof paymentFormSchema>;
